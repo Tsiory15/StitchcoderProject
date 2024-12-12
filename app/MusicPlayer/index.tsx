0cba3ae91deb8ad1496885d94d6a7b7631a26e3f
Tsiory15 <raphaeltsiory15@gmail.com>
2024-12-12 21:07:43 +0300

    test music player

---
+++ app/MusicPlayer/index.tsx
@@ -0,0 +1,63 @@
+import {View,Text,Button,StyleSheet} from 'react-native'
+import {Audio} from 'expo-av'
+import React,{ useState } from 'react'
+import Icon from '@expo/vector-icons/FontAwesome6'
+
+export default function Player () {
+    const [son,setSon] = useState<Audio.Sound | null>(null);
+    const [play,setPlay] = useState(false);
+
+    const Player = async () => {
+        if (!son){
+            const {sound:newSound} = await Audio.Sound.createAsync(
+                require('../public/Adele_Hello.mp3')
+            );
+            setSon(newSound);
+            await newSound.playAsync();
+            setPlay(true);
+
+            newSound.setOnPlaybackStatusUpdate((status) => {
+                if(status.isLoaded && status.didJustFinish){
+                    setPlay(false);
+                }
+            });
+        }else{
+            await son.playAsync();
+            setPlay(true);
+        }
+    };
+
+    const pause = async () => {
+        if(son){
+            await son.pauseAsync();
+            setPlay(false);
+        }
+    }
+
+    const stop = async () => {
+        if(son){
+            await son.stopAsync();
+            setPlay(false);
+        }
+    }
+    return(
+        <View style={styles.container}>
+            <Icon name={play ? 'pause' : 'play'} size={50} onPress={play ? pause : Player} />
+            <Icon name="stop" size={50} onPress={stop} />
+    </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+      alignSelf:'center',
+      gap:20
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 20,
+    },
+  });
+  
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import {View,StyleSheet} from 'react-native'
+import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <View>Icon</View>
+        <View>Icon</View>
+        <View>Icon</View>
+      </View>
+      <Slot/>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  header:{
+    height:50,
+    display:'flex',
+    justifyContent:'space-around',
+    alignItems:'center',
+    flexDirection:'row'
+  }
+})
